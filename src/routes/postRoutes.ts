598dd8f6fe0a2698cce95d88538bf0c5ab21c500
Elor-Itz <elor2200@gmail.com>
2025-01-12 16:49:53 +0200

    Added auth tests

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,15 +1,16 @@
 import express from "express";
 const router = express.Router();
 import postsController from "../controllers/postController";
+import { authMiddleware } from "../controllers/authController";
 
 router.get("/", postsController.getAll.bind(postsController));
 
 router.get("/:id", postsController.getById.bind(postsController));
 
-router.post("/", postsController.create.bind(postsController));
+router.post("/", authMiddleware, postsController.create.bind(postsController));
 
-router.put("/:id", postsController.updateItem.bind(postsController));
+router.put("/:id", authMiddleware, postsController.updateItem.bind(postsController));
 
-router.delete("/:id", postsController.deleteItem.bind(postsController));
+router.delete("/:id", authMiddleware, postsController.deleteItem.bind(postsController));
 
 export default router;
